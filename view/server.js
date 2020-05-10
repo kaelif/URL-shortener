935e4430846422a8c433903b9692b9fd084a9325
kaelif <flanagan.kaeli@gmail.com>
2020-05-10 11:48:07 -0700

    server

--- view/server.js
+++ view/server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://localhost/urlshortener', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
-app.get('/', (req, res) =>{
-    res.render('index')
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT || 5000);
